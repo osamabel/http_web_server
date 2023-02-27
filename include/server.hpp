f073e26e22cd9ee208985894e778337fa0301556
oussama belkhadir <osamaalpha1997@gmail.com>
2023-02-27 20:23:41 +0100

    add handlers

--- include/server.hpp
+++ include/server.hpp
@@ -6,12 +6,13 @@
 /*   By: obelkhad <obelkhad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 18:29:55 by obelkhad          #+#    #+#             */
-/*   Updated: 2023/02/26 19:05:21 by obelkhad         ###   ########.fr       */
+/*   Updated: 2023/02/27 18:25:13 by obelkhad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
 #include <iostream>
-#include <cstring>
 #include <fstream>
 #include <vector>
 #include <map>
@@ -27,40 +28,46 @@
 // "upload_dir"
 // "accepted_methods"
 // "client_body_max_size"
-// class Server
-// {
-// private:
-// 	int __id;
-// 	std::map<std::string, std::vector<std::string> > 	__config;
-// public:
-// 	Server(std::string &)
-// 	{
-// 	}
-// 	void __parse(std::string);
-// 	void __listen(std::string);
-// 	void __server_name(std::string);
-// 	void __cgi(std::string);
-// 	void __root(std::string);
-// 	void __index(std::string);
-// 	void __redirect(std::string);
-// 	void __autoindex(std::string);
-// 	void __error_page(std::string);
-// 	void __upload_dir(std::string);
-// 	void __accepted_methods(std::string);
-// 	void __client_body_max_size(std::string);
-	
-// 	~Server();
-// };
+
+
+class Server
+{
+private:
+	std::map<std::string, std::vector<std::string> > 	__config;
+public:
+	Server();
+	~Server();
+};
 
 class Web
 {
 private:
-	// std::vector<Server>	 		__servs;					//array of servers
-	std::ifstream				__file;
-	std::string					__config_path;
+	typedef void (Web::*handler)();
+	std::map<std::string, handler>		__handlers;
+	std::vector<Server>	 				__servers;			//array of servers
+	std::vector<std::string> 			__line_splited;
+	std::ifstream						__file;
+	bool								__curly_server;
+	bool								__curly_location;
+	bool								__location_zone;
+
+	void __server();
+	void __listen();
+	void __location();
+	void __server_name();
+	void __cgi();
+	void __root();
+	void __index();
+	void __redirect();
+	void __autoindex();
+	void __error_page();
+	void __upload_dir();
+	void __methods();
+	void __client_body_max_size();
 public:
-	Web(std::string &__config);
+	Web();
+	void __parse(std::string &__config_path);
+
 	~Web();
-	void __parse();
-	std::string __extract_parameters(std::string &old);
 };
+
