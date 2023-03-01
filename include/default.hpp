8e9a4f7230267554f34994fc5ed53bdf8745de4e
oussama belkhadir <osamaalpha1997@gmail.com>
2023-03-01 09:50:16 +0100

    add default config

---
+++ include/default.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   default.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: obelkhad <obelkhad@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 09:06:33 by obelkhad          #+#    #+#             */
+/*   Updated: 2023/03/01 09:24:40 by obelkhad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#define ADDRESS "0.0.0.0"
+#define PORT "8080"
+#define AUTOINDEX "off"
+#define INDEX "index.html"
+#define CLIENT_BODY_MAX_SIZE "100M"
